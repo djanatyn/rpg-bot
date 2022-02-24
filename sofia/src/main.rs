3613344be7385ea04fb41cfaab44e651f4a70726
Jonathan Strickland <djanatyn@gmail.com>
2022-02-24 13:56:52 -0500

    document rust code

--- sofia/src/main.rs
+++ sofia/src/main.rs
@@ -1,23 +1,39 @@
+//! Sofia (like the Dragon Quest IV Hero)!
+//!
+//! An experiment controlling an NES emulator using Rust.
+//! FCEUX lua APIs are used to read memory addresses and control input.
 use serde::{Deserialize, Serialize};
 use std::{io, str};
 use tokio::net::TcpStream;
 use tokio::process::Command;
 use tokio::time::{sleep, Duration};
 
+/// Path of FCEUX lua script, passed as argument to --loadlua.
 const LUA_SCRIPT: &str = "../final-fantasy.lua";
+/// Path of Final Fantasy 1 NES ROM.
 const ROM: &str = "../final-fantasy.zip";
+/// Hostname and port of FCEUX listening TCP socket.
 const FCEUX_SOCKET: &str = "localhost:8080";
+/// Time to wait between checking for FCEUX TCP socket.
 const FCEUX_START_WAIT: Duration = Duration::from_millis(1000);
+/// Time to wait between frames.
 const DELAY_BETWEEN_FRAMES: Duration = Duration::from_millis(16); // TODO: switch to interval?
 
+/// Value of a memory address read from FCEUX.
+///
+/// Received over network during emulation. Each address has a set of tags.
+/// Each address may be used for different purposes, in different situations
+/// during runtime.
 #[derive(Debug, Deserialize)]
 struct MemoryAddress {
     value: u64,
     tags: Vec<String>,
 }
 
-// https://fceux.com/web/help/LuaFunctionsList.html
-// (joypad library -> joypad.write)
+/// Virtual controller input.
+///
+/// Serialized into JSON to send to FCEUX.
+/// - <https://fceux.com/web/help/LuaFunctionsList.html>
 #[derive(Debug, Serialize, Clone, Copy)]
 struct Input {
     up: bool,
@@ -32,6 +48,22 @@ struct Input {
     select: bool,
 }
 
+/// Neutral input on the virtual controller (no buttons pressed).
+///
+/// Useful with struct update syntax:
+/// ```
+/// let game_input = Input { start: true, ..NEUTRAL };
+/// assert_eq(game_input, Input {
+///     up: false,
+///     down: false,
+///     left: false,
+///     right: false,
+///     a: false,
+///     b: false,
+///     start: true,
+///     select: false
+/// });
+/// ```
 const NEUTRAL: Input = Input {
     up: false,
     down: false,
@@ -43,8 +75,11 @@ const NEUTRAL: Input = Input {
     select: false,
 };
 
+/// Launch FCEUX and connect to lua socket. Read values of memory addresses and
+/// send serialized inputs in a loop.
 #[tokio::main]
 async fn main() -> io::Result<()> {
+    // spawn fceux, with listening lua server
     let mut fceux = Command::new("fceux")
         .arg("--loadlua")
         .arg(LUA_SCRIPT)
@@ -52,14 +87,14 @@ async fn main() -> io::Result<()> {
         .spawn()
         .expect("failed to spawn fceux");
 
+    // signal handler, child fceux process needs SIGKILL
     tokio::spawn(async move {
         tokio::signal::ctrl_c().await.unwrap();
         println!("sending kill signal to fceux");
         fceux.kill().await.expect("kill failed");
     });
 
-    let mut frame: u64 = 0;
-
+    // attempt to connect to fceux lua server
     let stream: TcpStream = loop {
         // wait one second so fceux can launch
         sleep(FCEUX_START_WAIT).await;
@@ -69,10 +104,12 @@ async fn main() -> io::Result<()> {
         };
     };
 
+    let mut frame: u64 = 0;
     loop {
         let mut buf = [0; 4096];
         let mut memory = String::new();
 
+        // read input from socket
         loop {
             stream.readable().await?;
             match stream.try_read(&mut buf) {
@@ -86,22 +123,29 @@ async fn main() -> io::Result<()> {
             }
         }
 
+        // parse live memory address values
         let json = memory.trim().lines().collect::<Vec<_>>();
         let addresses: Vec<MemoryAddress> = serde_json::from_str(json[0]).expect("failed to parse");
         println!("{addresses:#?}");
 
+        let num_addresses = addresses.len();
+        println!("rust: got {num_addresses:?} addresses (frame {frame})\n");
+
+        // press START on virtual controller
+        // TODO: dynamic input based on memory values (state machine)
+        let game_input = Input {
+            start: true,
+            ..NEUTRAL
+        };
+
+        // convert controller input to json
+        let input_json =
+            serde_json::to_string(&game_input).expect("failed to convert input to JSON");
+
+        // send controller input over socket
         loop {
             stream.writable().await?;
-            let num_addresses = addresses.len();
-            // let output = format!("rust: got {num_addresses:?} addresses (frame {frame})\n");
-            let game_input = Input {
-                start: true,
-                ..NEUTRAL
-            };
-            let input_json =
-                serde_json::to_string(&game_input).expect("failed to convert input to JSON");
             let output = format!("{input_json}\n");
-
             match stream.try_write(&output.into_bytes()) {
                 Ok(_) => break,
                 Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => continue,
